868d7cf8aec8ac98839db88c0126ea000eeb0e85
khkim <stakkh4@gmail.com>
2023-09-27 14:42:20 +0900

    BoardRepository

--- src/main/java/com/kkh/boardback/repository/BoardRepository.java
+++ src/main/java/com/kkh/boardback/repository/BoardRepository.java
@@ -11,8 +11,6 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
     // description : nativeQuery - 하나의 테이블사용에 대한 제한을 없애 수정,삭제//
     // description : read : view / cud : 쿼리 직접 작성 
         
-    // @Query(value = "", 
-    //     nativeQuery = true)
         
 
 }
