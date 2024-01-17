b8e32cfadf37054cbe198d73b9e96325931c308d
kkh <qr21@naver.com>
2024-01-17 10:17:12 +0900

    feat : 서비스 레이어 -> 아이디 체크에 대한 검증 로직
     아이디 체크에 대한 검증 로직

--- src/main/java/com/kkh/boardback/service/AuthService.java
+++ src/main/java/com/kkh/boardback/service/AuthService.java
@@ -3,12 +3,13 @@ package com.kkh.boardback.service;
 import org.springframework.http.ResponseEntity;
 
 import com.kkh.boardback.dto.request.auth.SignUpRequestDto;
+import com.kkh.boardback.dto.request.auth.IdCheckRequestDto;
 import com.kkh.boardback.dto.request.auth.SignInRequestDto;
+import com.kkh.boardback.dto.response.auth.IdCheckResponseDto;
 import com.kkh.boardback.dto.response.auth.SignInResponseDto;
 import com.kkh.boardback.dto.response.auth.SignUpResponseDto;
 
 public interface AuthService {
-    // ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto);  
-    ResponseEntity< ? super SignUpResponseDto> signUp(SignUpRequestDto dto);
-    ResponseEntity< ? super SignInResponseDto> signIn(SignInRequestDto dto);
+    ResponseEntity< ? super IdCheckResponseDto> idCheck(IdCheckRequestDto dto);
+
 }
--- src/main/java/com/kkh/boardback/service/implement/AuthServiceImplement.java
+++ src/main/java/com/kkh/boardback/service/implement/AuthServiceImplement.java
@@ -5,8 +5,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.kkh.boardback.dto.request.auth.IdCheckRequestDto;
 import com.kkh.boardback.dto.request.auth.SignInRequestDto;
 import com.kkh.boardback.dto.request.auth.SignUpRequestDto;
+import com.kkh.boardback.dto.response.auth.IdCheckResponseDto;
 import com.kkh.boardback.dto.response.auth.SignInResponseDto;
 import com.kkh.boardback.dto.response.auth.SignUpResponseDto;
 import com.kkh.boardback.dto.response.ResponseDto;
@@ -25,67 +27,23 @@ public class AuthServiceImplement implements AuthService{
     private final JwtProvider jwtProvider; 
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public ResponseEntity<? super IdCheckResponseDto> idCheck(IdCheckRequestDto dto) {
+
+        try {
+            String userId = dto.getId();
+            boolean existId = userRepository.existsById(userId);
+            if(existId) return IdCheckResponseDto.duplicateId();            
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+        return IdCheckResponseDto.success();
+    }
     
-    @Override
-    public ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto) {
-        
-        try {
 
-            String email = dto.getEmail();
-            String nickname = dto.getNickname();
-            String telNumber = dto.getTelNumber();
-            // description : 이메일중복,닉네임 중복  전화번호중복, 비밀번호 중복 //
-            boolean hasEmail = userRepository.existsByEmail(email);
-             if(hasEmail) return SignUpResponseDto.duplicatedEmail();
-             
-            boolean hasTelnumber = userRepository.existsByTelNumber(telNumber);
-             if(hasTelnumber) return SignUpResponseDto.duplicatedTelNumber();
-             
-            boolean hasNickname = userRepository.existsByNickname(nickname);
-            if(hasNickname) return SignUpResponseDto.duplicatedNickname();
 
-            String password = dto.getPassword();
-            String encodedPassword = passwordEncoder.encode(password);
 
-            // description: 비밀번호를 평문으로 된  암호화된 코드로 변경 //
-            dto.setPassword(encodedPassword);
-            UserEntity userEntity = new UserEntity(dto);
-            userRepository.save(userEntity);
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.databaseError();
-        }
-
-        
-        return SignUpResponseDto.success(); 
-    }
-
-    @Override
-    public ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto) {
-        String token = null;
-        // ! password 암호화된 password 평문의 password인지 비교  //
-        try {
-            
-            String email = dto.getEmail();
-
-            UserEntity userEntity = userRepository.findByEmail(email);
-            if(userEntity == null) return SignInResponseDto.signInfailed();
-
-            String password = dto.getPassword();
-            String encodedPassword = userEntity.getPassword();
-
-            boolean isMatched = passwordEncoder.matches(password, encodedPassword);
-            if(!isMatched) return SignInResponseDto.signInfailed();
-
-            token = jwtProvider.create(email);            
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return ResponseDto.databaseError();
-        }
-
-        return SignInResponseDto.success(token);
-    }
 
 }
