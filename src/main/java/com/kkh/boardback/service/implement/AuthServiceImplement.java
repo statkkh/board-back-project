08195b9b254a40985d9dbfae3e3bb0c9c9d74219
kkh <qr21@naver.com>
2024-01-18 12:43:53 +0900

    feat : 서비스 레이어 이메일 인증 검증로직
    이메일 인증 검증후 인증 엔터티에 저장하기및 응답에  대한  객체전달

--- src/main/java/com/kkh/boardback/service/implement/AuthServiceImplement.java
+++ src/main/java/com/kkh/boardback/service/implement/AuthServiceImplement.java
@@ -5,16 +5,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.kkh.boardback.common.CertificationNumber;
+import com.kkh.boardback.dto.request.auth.EmailCertificationRequestDto;
 import com.kkh.boardback.dto.request.auth.IdCheckRequestDto;
 import com.kkh.boardback.dto.request.auth.SignInRequestDto;
 import com.kkh.boardback.dto.request.auth.SignUpRequestDto;
+import com.kkh.boardback.dto.response.auth.EmailCertificationResponseDto;
 import com.kkh.boardback.dto.response.auth.IdCheckResponseDto;
 import com.kkh.boardback.dto.response.auth.SignInResponseDto;
 import com.kkh.boardback.dto.response.auth.SignUpResponseDto;
 import com.kkh.boardback.dto.response.ResponseDto;
+import com.kkh.boardback.entity.CertificationEntity;
 import com.kkh.boardback.entity.UserEntity;
+import com.kkh.boardback.provider.EmailProvider;
 import com.kkh.boardback.provider.JwtProvider;
 import com.kkh.boardback.service.AuthService;
+import com.kkh.boardback.repository.CertificationRepository;
 import com.kkh.boardback.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -23,8 +29,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthServiceImplement implements AuthService{
 
-    private final UserRepository userRepository;   
+    private final UserRepository userRepository;
+    private final CertificationRepository certificationRepository;
+    
     private final JwtProvider jwtProvider; 
+
+    private final EmailProvider emailProvider;
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -40,6 +50,33 @@ public class AuthServiceImplement implements AuthService{
             return ResponseDto.databaseError();
         }
         return IdCheckResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super EmailCertificationResponseDto> emailCertification(EmailCertificationRequestDto dto) {
+        
+        try {
+            
+            String userId = dto.getId();
+            String email = dto.getEmail();
+
+            boolean isExistId = userRepository.existsByUserId(userId);
+            if(isExistId) return EmailCertificationResponseDto.duplicateId();
+
+            String certificationNumber = CertificationNumber.getCertificationNumber();
+
+            boolean isSuccess = emailProvider.sendCertificationMail(email, certificationNumber);
+            if(!isSuccess) return EmailCertificationResponseDto.mailSendFail();
+
+            CertificationEntity certificationEntity = new CertificationEntity(userId, email,  certificationNumber);
+            certificationRepository.save(certificationEntity);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return EmailCertificationResponseDto.success();
     }
     
 
