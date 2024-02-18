70c81640dfe170c7fa0c118d1734289bdeb97d64
kkh <qr21@naver.com>
2024-02-18 12:41:52 +0900

    feat :파일 다운로드 기능을 상속할 인터페이스 생성

--- src/main/java/com/kkh/boardback/service/FileService.java
+++ src/main/java/com/kkh/boardback/service/FileService.java
@@ -2,11 +2,15 @@ package com.kkh.boardback.service;
 
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface FileService {
     
     String upload(MultipartFile file);
     Resource getFile(String fileName);
     
+
+    void download(String fileUrl, HttpServletResponse response);
+
     
 }
