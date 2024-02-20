8de1a37e7f194f9e67f5628b302a52c5321687b5
kkh <qr21@naver.com>
2024-02-20 09:43:40 +0900

    43th commit feat : 뷰에게  전달한 파일 다운로드에 대한 엔드포인트 추가
    Get 메서드를 통해 엔드포인트를 조회한 파일을 비즈니스 로직에 성공시 파일 다운로드

--- src/main/java/com/kkh/boardback/controller/FileController.java
+++ src/main/java/com/kkh/boardback/controller/FileController.java
@@ -1,25 +1,35 @@
 package com.kkh.boardback.controller;
 
+import com.kkh.boardback.service.FileService;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.core.io.Resource;
+
 import org.springframework.http.MediaType;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.multipart.MultipartFile;
 
-import com.kkh.boardback.service.FileService;
+import jakarta.servlet.http.HttpServletResponse;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/file")
-@RequiredArgsConstructor
+ @RequiredArgsConstructor
 public class FileController {
     // 파일 서비스 구현되지 않아도 사용 가능 추상화 의 장점
     private final FileService fileService;
+
+    @Value("${file.path}")
+    private String filePath;
 
     @PostMapping("/upload")
     public String upload(
@@ -29,14 +39,22 @@ public class FileController {
         return url;
     }
 
-    // description : 파일 불러오기 
-    @GetMapping(value = "{fileName}", produces = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
+    @GetMapping(value="{fileName}", produces={MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
     public Resource getFile(
         @PathVariable("fileName") String fileName
-    ){  
+    ) {
         Resource resource = fileService.getFile(fileName);
         return resource;
-    } 
+    }    
 
+    // description : 파일 불러오기 
+    @GetMapping("/download/{fileUrl}")
+    public void downloadFile(
+        @PathVariable String fileUrl,
+        HttpServletResponse response){
+        
+        // 파일 서비스를 사용하여 파일 다운로드
+        fileService.download(fileUrl, response);
+    }
 
 }
